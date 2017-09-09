212743f7781ed3b851bebe40de385784ac359f34
fedchishina.victoria@gmail.com <fedchishina.victoria@gmail.com>
2017-09-09 18:28:28 +0300

    made routes, function of controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        return User::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+
+        return $user;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,24 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//----------------- groups  --------------------//
+//fetch list of groups
+Route::get('/groups', 'GroupController@index');
+//create a group
+Route::post('/groups', 'GroupController@store');
+//fetch info of a group
+Route::get('/groups/{id}', 'GroupController@show');
+//modify group info
+Route::put('/groups/{id}', 'GroupController@update');
+
+//---------------- users  ---------------------//
+//fetch list of users
+Route::get('/users', 'UserController@index');
+//create a user
+Route::post('/users', 'UserController@store');
+//fetch info of a user
+Route::get('/users/{id}', 'UserController@show');
+//modify user info
+Route::put('/users/{id}', 'UserController@update');
